caca8fb17f2c128fd8d836f10c8b050862714ead
Chien <stdnt25@gmail.com>
2019-03-11 13:22:07 +0800

    larger and larger button1

--- test0311_Hello/test0311_Hello/Form1.Designer.cs
+++ test0311_Hello/test0311_Hello/Form1.Designer.cs
@@ -30,7 +30,7 @@
             // 
             this.button1.Location = new System.Drawing.Point(57, 29);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(213, 41);
+            this.button1.Size = new System.Drawing.Size(255, 57);
             this.button1.TabIndex = 0;
             this.button1.Text = "button1";
             this.button1.UseVisualStyleBackColor = true;
