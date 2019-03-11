41a8c5efeaec257c6f2fa78fecc239178c0f4680
Chien <stdnt25@gmail.com>
2019-03-11 13:50:16 +0800

    button2 add comment

--- test0311_Hello/test0311_Hello/Form1.cs
+++ test0311_Hello/test0311_Hello/Form1.cs
@@ -20,6 +20,7 @@ namespace test0311_Hello {
 
         private void button2_Click(object sender, EventArgs e) {
             button2.Text = DateTime.Now.ToString();
+            // edit on Master branch
         }
     }
 }
